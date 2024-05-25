75ce52692d131bafbb89575be0f5701bbb715d6a
Enes Akar <enesakar2626@gmail.com>
2024-05-25 17:35:39 +0300

    Finished with recursion

---
+++ Binary Search Trees/BST Insertion/Solution.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+	public static Node insert(Node root,int data) {
+        // if current root is empty, replace it.
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        
+        //insert to left
+        //update the left branch
+        else if (root.data >= data) {
+            root.left = insert(root.left, data);
+        }
+
+        //insert to right
+        //update the right branch
+        else {
+            root.right = insert(root.right, data);
+        }
+    
+        return root;
+    }
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
